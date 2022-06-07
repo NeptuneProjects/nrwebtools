d50cdca8dcdb74d3f5941c933b93db0d8dae130a
NeptuneProjects <william.f.jenkins@gmail.com>
2022-06-06 19:20:33 -0700

    Update .gitignore

--- nrwebtools/settings.py
+++ nrwebtools/settings.py
@@ -17,7 +17,7 @@ from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
