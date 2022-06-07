db86e102931669af96a854971e36afa47c71f244
NeptuneProjects <william.f.jenkins@gmail.com>
2022-06-06 21:08:20 -0700

    Update about

--- home/views.py
+++ home/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-import datetime
 
 TEMPLATE_DIRS = (
     'os.path.join(BASE_DIR, "templates")'
 )
 
 def index(request):
-    today = datetime.datetime.now().date()
-    context = {"today": today}
-    return render(request, "index.html", context)
+    # today = datetime.datetime.now().date()
+    # context = {"today": today}
+    return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
