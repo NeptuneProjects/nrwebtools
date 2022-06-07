ff8b62f7d805217f15164a9840de66483ea67870
NeptuneProjects <william.f.jenkins@gmail.com>
2022-06-07 02:21:13 -0700

    fix urls

--- nrwebtools/urls.py
+++ nrwebtools/urls.py
@@ -17,8 +17,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('home/', include('home.urls')),
+    # path('home/', include('home.urls')),
+    # path('about/', include('home.urls')),
     path('block29app/', include('block29app.urls'))
 ]
