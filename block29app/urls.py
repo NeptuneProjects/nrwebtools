53174cec222561a3a7e225e08e6844d18836ce97
NeptuneProjects <william.f.jenkins@gmail.com>
2022-07-18 23:06:58 -0700

    Remove extraneous import

--- block29app/urls.py
+++ block29app/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from .views import Block29View
 from . import views
 
 urlpatterns = [
-    path('', Block29View.as_view(), name='/block29app/'),
+    path('', views.Block29View.as_view(), name='/block29app/'),
 ]
